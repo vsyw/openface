6e74189fb065468a8e2fcec8152fd6f12cf90bf2
victor wang <iwantooxxoox@gmail.com>
2017-09-17 17:27:13 +0800

    Initial Commit

---
+++ openface/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  openface
+//
+//  Created by victor.sy_wang on 2017/9/17.
+//  Copyright © 2017年 victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ openfaceTests/openfaceTests.swift
@@ -0,0 +1,36 @@
+//
+//  openfaceTests.swift
+//  openfaceTests
+//
+//  Created by victor.sy_wang on 2017/9/17.
+//  Copyright © 2017年 victor. All rights reserved.
+//
+
+import XCTest
+@testable import openface
+
+class openfaceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
